faf36c4ac5df457e9b12bdf98bfc9a02b1f47752
Pavan S <54991142+pavanshetty-in@users.noreply.github.com>
2021-09-06 20:03:06 +0530

    pagination added

--- app/controllers/allposts_controller.rb
+++ app/controllers/allposts_controller.rb
@@ -1,6 +1,8 @@
 class AllpostsController < ApplicationController
+    Post_per_page=5
     def index
-        @allposts =Post.all
+        @page=params.fetch(:page,0).to_i
+        @allposts =Post.offset(@page * Post_per_page).limit(Post_per_page)
     end
 
 end
